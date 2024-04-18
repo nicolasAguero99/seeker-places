709a652f47067320216c146228df3d7f6354f7eb
nicolasAguero99 <nico.agueroo99@gmail.com>
2024-04-18 20:44:13 -0300

    Fix deploy 5

--- api/scraping.py
+++ api/scraping.py
@@ -15,8 +15,9 @@ URL_BASE = 'https://www.google.com/maps/'
 
 def test_scraping():
   chrome_options = Options()
-  chrome_options.binary_location = "/path/to/chrome/binary"
+  # chrome_options.binary_location = "/path/to/chrome/binary"
   chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--lang=es")
   # try: driver = webdriver.Chrome()
   try: driver = webdriver.Chrome(options=chrome_options)
   except Exception as e:
@@ -39,11 +40,15 @@ def test_scraping():
 # def web_scraping(url_web: str):
 def web_scraping(location: str, places_length: int):
   chrome_options = Options()
-  chrome_options.binary_location = "/path/to/chrome/binary"
-  chrome_options.add_argument("--headless")  # Para ejecutar en modo sin cabeza, por ejemplo
+  # chrome_options.binary_location = "/path/to/chrome/binary"
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--lang=es")
 
   print('\n--------------\n')
-  driver = webdriver.Chrome(options=chrome_options)
+  try: driver = webdriver.Chrome(options=chrome_options)
+  except Exception as e:
+    print("Error al iniciar el controlador de Chrome:", e)
+    driver = None
   # driver = webdriver.Chrome()
   driver.set_window_size(1000, 880)
   driver.get(URL_BASE)
